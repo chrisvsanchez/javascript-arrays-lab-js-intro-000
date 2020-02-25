f0ec4d0979c83c56c90f998b3e756dc2aef26e46
Chris Sanchez <chrisvsanchez@gmail.com>
2020-02-25 04:48:47 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -33,6 +33,6 @@ function removeFirstKitten(){
 }
 
 function removeLastKitten(){
- let arr =  kittens.slice(0, kittens.length-1);
- return arr;
+ let kittens =  kittens.slice(0, kittens.length-1);
+ return kittens;
 }
