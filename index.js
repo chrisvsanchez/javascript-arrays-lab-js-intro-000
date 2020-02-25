22aa79d04a06ccb06e6d1f73ef87bbcc4fd7ab64
Chris Sanchez <chrisvsanchez@gmail.com>
2020-02-25 04:40:59 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ function appendKitten(name){
 }
 
 function prependKitten(name){
-  return kittens.slice(0, arra.length -1);
+  return kittens.concat(name);
 }
 function removeLastKitten(){
   return kittens.unshift();
